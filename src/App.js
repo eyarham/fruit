2f681d478093065a134042112768a21f3f7b81bb
Eric Yarham <eric.yarham@gmail.com>
2022-11-09 19:59:12 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import logo from './logo.svg';
 
 function App() {
+  const [show, setShow] = useState(false);
+  const onButtonClick = () => {
+    setShow(!show);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. or don't, whatever
         </p>
+        {show && <p>now u see me</p>}
+        {!show && <p>now u dont?</p>}
+        <a href="http://legalaid.org">legal aid</a>
+        <a href="http://legalaid.org">legal aid</a>
+        <a href="http://legalaid.org">legal aid</a>
+        <a href="http://legalaid.org">legal aid</a>
+        <a href="http://legalaid.org">legal aid</a>
+        <button onClick={onButtonClick}>switch</button>
+        <p>hello to all my friends welcome to my internet location</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React queen
         </a>
       </header>
     </div>
